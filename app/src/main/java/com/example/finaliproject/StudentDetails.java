1e47ff146e02d3ca59637a2a370ce7100832c776
JustineMsosa <bed-com-03-17@cc.ac.mw>
2021-09-04 01:46:53 -0700

    studentDetails done

--- app/src/main/java/com/example/finaliproject/StudentDetails.java
+++ app/src/main/java/com/example/finaliproject/StudentDetails.java
@@ -3,7 +3,10 @@ package com.example.finaliproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.finaliproject.Model.Student;
@@ -11,7 +14,8 @@ import com.example.finaliproject.Model.Student;
 import java.util.ArrayList;
 
 public class StudentDetails extends AppCompatActivity {
-    TextView name, age, sex, code, village;
+    TextView name, age, attendance, code, village;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +27,33 @@ public class StudentDetails extends AppCompatActivity {
 
         name = (TextView) findViewById(R.id.TextViewName);
         age = (TextView) findViewById(R.id.TextViewAge);
-        sex = (TextView) findViewById(R.id.student_sex);
+        attendance = (TextView) findViewById(R.id.TextViewSex);
+        village = (TextView) findViewById(R.id.TextViewVillage);
         code = (TextView) findViewById(R.id.TextViewCode);
+        imageView = (ImageView) findViewById(R.id.imageViewStu);
         code.setText("Student Id Number: 0012"+id);
+
+        Cursor cursor = MainActivity2.sqLiteHelper.getData("SELECT * FROM STUDENT");
+        while (cursor.moveToNext()) {
+            int id1 = cursor.getInt(0);
+            String name1 = cursor.getString(1);
+            String age1 = cursor.getString(2);
+            byte[] image1 = cursor.getBlob(4);
+            String present1 = cursor.getString(3);
+            String sex1 = cursor.getString(5);
+            String village1 = cursor.getString(6);
+            Integer studentCode1 = cursor.getInt(7);
+            if (id1 == id){
+                name.setText("  Name: "+name1);
+                age.setText("   Age: "+age1+"  | Sex: "+sex1);
+                attendance.setText("Attendance: "+present1);
+                village.setText("  Village: "+village1);
+                code.setText("  Student Code: 0"+id1+""+studentCode1);
+                imageView.setImageBitmap( BitmapFactory.decodeByteArray(image1, 0 , image1.length));
+
+            }
+        }
+
 
     }
 }
